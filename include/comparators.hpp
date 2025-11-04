76f47902399959610876ca45cebc6223b9d8e38b
Alex <kelheor23@gmail.com>
2025-11-04 17:23:47 +0300

    add book comparators

--- include/comparators.hpp
+++ include/comparators.hpp
@@ -4,6 +4,40 @@
 
 namespace bookdb::comp {
 
-struct LessByAuthor {};
+struct LessByAuthor {
+    bool operator()(const Book &a, const Book &b) const {
+        return a.author < b.author;
+    }
+};
+
+struct LessByTitle {
+    bool operator()(const Book &a, const Book &b) const {
+        return a.title < b.title;
+    }
+};
+
+struct LessByYear {
+    bool operator()(const Book &a, const Book &b) const {
+        return a.year < b.year;
+    }
+};
+
+struct LessByGenre {
+    bool operator()(const Book &a, const Book &b) const {
+        return a.genre < b.genre;
+    }
+};
+
+struct LessByRating {
+    bool operator()(const Book &a, const Book &b) const {
+        return a.rating < b.rating;
+    }
+};
+
+struct LessByReadCount {
+    bool operator()(const Book &a, const Book &b) const {
+        return a.read_count < b.read_count;
+    }
+};
 
 }  // namespace bookdb::comp
