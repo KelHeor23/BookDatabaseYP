1f3571fe4188b63e390bb65b135c82bfbde490b6
Alex <kelheor23@gmail.com>
2025-11-06 08:55:32 +0300

    start statisctic

--- include/statsistics.hpp
+++ include/statsistics.hpp
@@ -5,6 +5,7 @@
 #include <random>
 #include <stdexcept>
 #include <string_view>
+#include <flat_map>
 
 #include "book_database.hpp"
 
@@ -13,6 +14,34 @@
 namespace bookdb {
 
 template <BookContainerLike T, typename Comparator = TransparentStringLess>
-auto buildAuthorHistogramFlat(const BookDatabase<T> &cont, Comparator comp = {}) {}
+auto buildAuthorHistogramFlat(const BookDatabase<T> &cont, Comparator comp = {}) {
+    using flat_map = std::flat_map<std::string, std::size_t, Comparator>;
+
+    flat_map gist{comp};
+
+    for (const auto& it : cont.GetBooks())
+        ++gist[it.author];
+
+    return gist;
+}
+
+template <BookContainerLike T>
+auto calculateGenreRating(const BookDatabase<T> &cont) {
+    if (cont.GetBooks().empty()) return 0.;
+    auto &books = cont.GetBooks();
+    auto rating = std::transform(books.begin(), books.end(), [](auto &b){return b.rating;});
+    double sum = std::accumulate(rating.begin(), rating.end(), 0.);
+    return sum / books.size();
+}
+
+
+template <BookContainerLike T>
+auto calculateAverageRating(const BookDatabase<T> &cont) {
+    if (cont.GetBooks().empty()) return 0.;
+    auto &books = cont.GetBooks();
+    auto rating = std::transform(books.begin(), books.end(), [](auto &b){return b.rating;});
+    double sum = std::accumulate(rating.begin(), rating.end(), 0.);
+    return sum / books.size();
+}
 
 }  // namespace bookdb
