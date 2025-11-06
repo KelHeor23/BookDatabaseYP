a4970f291f8530ad56938da268f5cd0470cd1b25
Alex <kelheor23@gmail.com>
2025-11-06 08:05:17 +0300

    add BookDatabase tests

--- tests/db_test.cpp
+++ tests/db_test.cpp
@@ -1,3 +1,82 @@
 #include <gtest/gtest.h>
+#include "book_database.hpp"
 
-TEST(TestComponentName, SimpleCheck) { EXPECT_EQ(1 + 1, 2); }
+TEST(BookDatabaseTest, BookDatabasePushBack) {
+    bookdb::BookDatabase<> db;
+    bookdb::Book b1{
+        "Dune",
+        "Frank Herbert",
+        bookdb::Genre::SciFi,
+        1965,
+        5.0,
+        10
+    };
+
+    bookdb::Book b2{
+        "Dune Messiah",
+        "Frank Herbert",
+        bookdb::Genre::SciFi,
+        1969,
+        4.7,
+        5
+    };
+
+    db.PushBack(b1);
+    db.PushBack(b2);
+
+    EXPECT_EQ(db.size(), 2u);
+    EXPECT_EQ(db.GetAuthors().size(), 1u);
+
+    const auto& author = *db.GetAuthors().begin();
+    EXPECT_EQ(db.GetBooks()[0].author, author);
+    EXPECT_EQ(db.GetBooks()[1].author, author);
+}
+
+TEST(BookDatabaseTest, BookDatabaseInitList) {
+    bookdb::Book b1{
+        "Dune",
+        "Frank Herbert",
+        bookdb::Genre::SciFi,
+        1965,
+        5.0,
+        10
+    };
+
+    bookdb::Book b2{
+        "Dune Messiah",
+        "Frank Herbert",
+        bookdb::Genre::SciFi,
+        1969,
+        4.7,
+        5
+    };
+
+    bookdb::BookDatabase<> db{ b1, b2 };
+
+    EXPECT_EQ(db.size(), 2u);
+    EXPECT_EQ(db.GetAuthors().size(), 1u);
+
+    const auto& author = *db.GetAuthors().begin();
+    EXPECT_EQ(db.GetBooks()[0].author, author);
+    EXPECT_EQ(db.GetBooks()[1].author, author);
+}
+
+TEST(BookDatabaseTest, BookDatabaseEmplaceBack) {
+    bookdb::BookDatabase<> db;
+
+    auto& b = db.EmplaceBack(
+        "Dune",
+        "Frank Herbert",
+        bookdb::Genre::SciFi,
+        1965,
+        5.0,
+        10
+    );
+
+    EXPECT_EQ(db.size(), 1u);
+    EXPECT_EQ(db.GetAuthors().size(), 1u);
+
+    const auto& author = *db.GetAuthors().begin();
+    EXPECT_EQ(b.author, author);
+    EXPECT_EQ(db.GetBooks()[0].author, author);
+}
